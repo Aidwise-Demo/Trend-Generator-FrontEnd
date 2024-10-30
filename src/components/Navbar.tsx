0b8a98a49a3b82676736e353f4713332df1b7fff
Aidwise-Demo <demo@aidwise.ai>
2024-10-30 11:12:59 +0530

    svg to png

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div className="flex items-center gap-3">
           <Menu className="w-5 h-5" />
           <Image
-            src={"/assets/aarogya_ganit.svg"}
+            src={"/assets/Aarogya_Ganit_Logo-removebg-preview.png"}
             width={150}
             height={150}
             alt="Aarogya Ganit"
