d8084cf00d07bfe39c8a150f3c80943f176823a9
Aidwise-Demo <demo@aidwise.ai>
2024-10-30 11:37:23 +0530

    copied public to root folder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,7 +40,7 @@ const Navbar = () => {
         <div className="flex items-center gap-3">
           <Menu className="w-5 h-5" />
           <Image
-            src="/assets/Aarogya_Ganit_Logo-removebg-preview.png"
+            src="\assets\Aarogya_Ganit_Logo-removebg-preview.png"
             width={150}
             height={150}
             alt="Aarogya Ganit"
